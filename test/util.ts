e968265f91461d7e1588d3643a8bb957c5187b35
El Pupi <a01633814@unet.univie.ac.at>
2023-09-09 13:08:19 +0200

    computer reinstallation files metadata changed

--- test/util.ts
+++
@@ -1,8 +0,0 @@
-export const mockPrompts = (answers: Promise<{ [ K: string ]: any; }>[]) => {
-    const mockPrompts = answers.reduce((fn, answer) => fn.mockReturnValueOnce(answer), jest.fn());
-
-    return jest.mock('prompts', jest.fn().mockImplementation(() => ({
-        __esModule: true,
-        default: mockPrompts
-    })));
-};
